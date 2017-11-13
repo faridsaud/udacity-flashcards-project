eebeb6410115c64f8c384cbb0fc27e9c2ba1b199
Farid Saud <faridsaud93@gmail.com>
2017-11-13 15:06:59 -0500

    feat: Add style to deck detail

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -17,9 +17,23 @@ class DeckDetail extends Component {
         const { params } = this.props.navigation.state;
         return (
             <View style={styles.container}>
-                <Text style={styles.title}>{params.title}</Text>
-                <Text>{params.length} cards</Text>
+                <View style={styles.contentContainer}>
+                    <Text style={styles.title}>{params.title}</Text>
+                    <Text>{params.questions.length} cards</Text>
+                </View>
+
                 <View>
+                    <TouchableOpacity style = {styles.addCardBtn}>
+                        <Text style = {styles.addCardTxt} >
+                            Add Card
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style = {styles.startQuizBtn}>
+                        <Text  style = {styles.startQuizTxt}>
+                            Start Quiz
+                        </Text>
+                    </TouchableOpacity>
+
                 </View>
             </View>
         )
@@ -39,11 +53,39 @@ const styles = StyleSheet.create({
         borderBottomWidth:40,
         minWidth:'100%'
     },
+    contentContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
     title: {
         fontSize: 36,
         fontWeight: 'bold'
     },
     description:{
         fontSize:24
+    },
+    addCardBtn:{
+        padding: 10,
+        margin:5,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#000'
+    },
+    addCardTxt:{
+        fontWeight: 'bold'
+    },
+    startQuizBtn:{
+        padding: 10,
+        margin:5,
+        backgroundColor: '#000',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#000'
+    },
+    startQuizTxt:{
+        fontWeight: 'bold',
+        color:'#fff'
     }
 });
